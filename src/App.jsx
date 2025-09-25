e4d7d0016e39c69be6d0948819aea6c3a1a9808f
helenpastras <helenapastras@hotmail.com>
2025-09-25 10:28:58 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar/NavBar';
+import './App.css'
+
+const App = () => {
+  
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, friend!</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar =() => {
+    return(
+        <nav>
+            <p>NavBar contens wiil go here</p>
+        </nav>
+    );
+};
+
+export default NavBar;
